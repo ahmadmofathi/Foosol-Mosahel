5753cfdef9fda2812c8049eafb09c308df25abb4
Abdelrahamn <abdelrahmanahmedkhalifa99@gmail.com>
2025-01-05 10:39:54 +0200

    update prod vaiable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
-    production: true,
-    apiUrl:'api/'
+  production: true,
+  apiUrl: 'https://teeefa-001-site1.ntempurl.com/api/',
+  resourceUrl: 'https://teeefa-001-site1.ntempurl.com/',
 };
